1be72848e891c460652917115c26dee866f7da52
Eli Boninger <eboninger@gmail.com>
2025-05-17 14:58:27 -0400

    Add auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_17_180137) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_17_185305) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -38,6 +38,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_17_180137) do
     t.index ["user_group_id"], name: "index_lists_on_user_group_id"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
   create_table "user_groups", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -45,13 +54,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_17_180137) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_group_id", null: false
-    t.index ["user_group_id"], name: "index_users_on_user_group_id"
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
   add_foreign_key "lists", "user_groups"
-  add_foreign_key "users", "user_groups"
+  add_foreign_key "sessions", "users"
 end
